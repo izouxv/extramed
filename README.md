3fa33967aeb2db2b3fae53a258d21aaa8978c3ad
ralph baesso <WO7Q/xWOa7wTplkZj6l8Gx6j9jqDYsqQX5qXevyHsKE=>
2022-04-01 21:56:52 -0300

    Configurações iniciais

---
+++ README.md
@@ -0,0 +1,13 @@
+# Instalação do Rancher
+
+O Rancher deve estar numa máquina isolada. Não pode ficar dentro do Cluster
+
+Rodar o script do arquivo *rancher/rancher.sh*
+
+Setar a senha no Rancher
+
+Criar o cluster pela interface do Rancher
+
+# Instalação Traefic
+
+
---
+++ rancher/rancher.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl https://releases.rancher.com/install-docker/19.03.sh | sh
+sudo usermod -aG docker administrador
+docker run -d --restart=unless-stopped \
+  -p 80:80 -p 443:443 \
+  --privileged \
+  -v /opt/rancher:/var/lib/rancher \
+  rancher/rancher:stable
+
+#  --no-cacerts. Precicei deste comando no loadbalancer da AWS
+
